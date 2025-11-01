d032330994f36cbd84ba89b6c11da356aefd08d5
nosousa <nsousa89@gmail.com>
2025-11-01 11:40:12 -0600

    add generate pages recursively

--- src/generatepage.py
+++ src/generatepage.py
@@ -1,6 +1,7 @@
 import os
 from md_to_html import markdown_to_html_node
 from extracttitle import extract_title
+from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -20,3 +21,13 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, 'w') as f:
         f.write(html_page)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        src_path = os.path.join(dir_path_content, filename)
+        dst_path = os.path.join(dest_dir_path, filename)
+        if os.path.isdir(src_path):
+            generate_pages_recursive(src_path, template_path, dst_path)
+        elif os.path.isfile(src_path) and filename.endswith(".md"):
+            generate_page(src_path, template_path, Path(dst_path).with_suffix(".html"))
--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 import os
 import shutil
 from copystatic import recursive_copy
-from generatepage import generate_page
+from generatepage import generate_page, generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
-    
+
     recursive_copy(dir_path_static, dir_path_public)
-    generate_page("content/index.md", "template.html", "public/index.html")
-    
+
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
 
 main()
